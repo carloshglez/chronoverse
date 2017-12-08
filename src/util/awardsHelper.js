8c8c68382495ded40f04b0bd6b66c8a784ff67ce
Carlos Gonzalez <carlos-horacio.gonzalez-gordillo@hpe.com>
2017-12-08 12:28:24 -0600

    Add award helper with an the awards array

---
+++ src/util/awardsHelper.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import FaTrophy from 'react-icons/lib/fa/trophy'
+
+/**
+ *
+ */
+export const AWARDS = [
+    {
+        id: 1,
+        icon: <FaTrophy />,
+        text: 'Hello World!',
+        won: false
+    },
+    {
+        id: 2,
+        icon: <FaTrophy />,
+        text: 'Hello World!',
+        won: false
+    },
+    {
+        id: 3,
+        icon: <FaTrophy />,
+        text: 'Hello World!',
+        won: true
+    },
+    {
+        id: 4,
+        icon: <FaTrophy />,
+        text: 'Hello World!',
+        won: false
+    },
+    {
+        id: 5,
+        icon: <FaTrophy />,
+        text: 'Hello World!',
+        won: true
+    },
+];
--- src/views/Awards.js
+++ src/views/Awards.js
@@ -7,6 +7,7 @@ import MdHighlightRemove from 'react-icons/lib/md/highlight-remove'
 import FaTrophy from 'react-icons/lib/fa/trophy'
 import FaCheckSquareO from 'react-icons/lib/fa/check-square-o'
 import { isPassive, isMobileDevice } from '../util/helpers';
+import { AWARDS } from '../util/awardsHelper';
 
 export default class Awards extends React.Component {
     constructor() {
@@ -36,45 +37,31 @@ export default class Awards extends React.Component {
     }
 
     render() {
+        let awardItems = AWARDS.map(function (item) {
+            return (
+                <li key={item.id}>
+                    <div className='icon-award'>{item.icon}</div>
+                    <div className='text-award'>{item.text}</div>
+                    <div className='status-award'>{(item.won) ? <FaCheckSquareO /> : null} </div>
+                </li>
+            )
+        });
+
         return (
             <div>
                 <ul className='gameAwards'>
                     <li>
-                       <MdHighlightRemove onClick={this.props.setIntro} />
+                        <MdHighlightRemove onClick={this.props.setIntro} />
                     </li>
                 </ul>
                 <div className='selectgame'>
-					<h3> <FaTrophy/> Your Awards:</h3>
-				</div>
+                    <h3> <FaTrophy /> Your Awards:</h3>
+                </div>
                 <div>
                     <div id='wrapper' className='wrapper-award' ref='wrapper'>
                         <div id='scroller' className='scroller-award'>
                             <ul>
-                                <li>
-                                    <div className='icon-award'>O</div>
-                                    <div className='text-award'>Hello world!</div>
-                                    <div className='status-award'> <FaCheckSquareO/> </div>
-                                </li>
-                                <li>
-                                    <div className='icon-award'>O</div>
-                                    <div className='text-award'>Hello world!</div>
-                                    <div className='status-award'> <FaCheckSquareO/> </div>
-                                </li>
-                                <li>
-                                    <div className='icon-award'>O</div>
-                                    <div className='text-award'>Hello world!</div>
-                                    <div className='status-award'> <FaCheckSquareO/> </div>
-                                </li>
-                                <li>
-                                    <div className='icon-award'>O</div>
-                                    <div className='text-award'>Hello world!</div>
-                                    <div className='status-award'> <FaCheckSquareO/> </div>
-                                </li>
-                                <li>
-                                    <div className='icon-award'>O</div>
-                                    <div className='text-award'>Hello world!</div>
-                                    <div className='status-award'> <FaCheckSquareO/> </div>
-                                </li>
+                                {awardItems}
                             </ul>
                         </div>
                     </div>
